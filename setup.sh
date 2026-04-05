4c4fbd24670de17026a003e15e6f50564f08a981
Xue J. Zhao <217244780+zxjrsch@users.noreply.github.com>
2026-04-05 15:05:54 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@ mkdir data && cd data
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 curl -LsSf https://hf.co/cli/install.sh | bash
-
-uv init --python 3.13
 uvx hf auth login
-uv add boto3 botocore[crt] matplotlib numpy requests tiktoken tqdm pandas pyarrow huggingface_hub
+uv init --python 3.13
 
+uv add boto3 botocore[crt] matplotlib numpy requests tiktoken tqdm pandas pyarrow huggingface_hub omegaconf loguru
 sudo yum install htop -y
 
 aws login
+
 aws configure set region us-east-1
 aws configure
 aws configure list
